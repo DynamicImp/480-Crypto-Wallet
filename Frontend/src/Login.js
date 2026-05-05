e0140ba05825f920a7338c1ca75cd662e7961360
DynamicImp <veric0156@gmail.com>
2026-05-05 22:24:55 +0000

    Changed port fetch

--- Frontend/src/Login.js
+++ Frontend/src/Login.js
@@ -6,7 +6,7 @@ function Login({ onLoginSuccess }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/login', {
+    const response = await fetch('https://reimagined-yodel-97jx5jgw5p9427v55-5000.app.github.dev/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
